c3ee4b0ce481c9c18c871ea1c247040c89e4f305
a-Zszywacz <Teresher935@GMail.com>
2019-05-08 20:02:02 +0200

    After merge from feature/chowrat to master another

--- app/src/main/java/com/example/price_comparison/Database.java
+++ app/src/main/java/com/example/price_comparison/Database.java
@@ -3,9 +3,22 @@ package com.example.price_comparison;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.price_comparison.api.ApiController;
+import com.example.price_comparison.api.Product;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
 //import android.widget.Toolbar;
 
 public class Database extends AppCompatActivity {
+
+    private ArrayAdapter<String> arrayAdapter;
+    private ApiController apiController = new ApiController();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,23 @@ public class Database extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //setActionBar(toolbar);
+        ArrayList<String> text = new ArrayList<>();
+        ArrayList<Product> products = new ArrayList<>();
+        try {
+            products = apiController.getAllProducts();
+
+            for (Product product : products){
+                text.add(product.getName());
+            }
+            text.add("dupa");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ListView listView = findViewById(R.id.listview_database);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.scanner_list, text);
+        listView.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/price_comparison/MainActivity.java
+++ app/src/main/java/com/example/price_comparison/MainActivity.java
@@ -1,143 +1,29 @@
 package com.example.price_comparison;
 
-import com.example.price_comparison.api.*;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    public void scanBarcode(View view) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-        integrator.setPrompt("Scan");
-        integrator.setCameraId(0);
-        integrator.setBeepEnabled(false);
-        integrator.setBarcodeImageEnabled(false);
-        integrator.initiateScan();
-    }
-
-    @Override
-    public void onActivityResult(int RequestCode, int ResultCode, Intent data){
-        IntentResult result = IntentIntegrator.parseActivityResult(RequestCode, ResultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Log.d("MainActivity", "Cancelled scan");
-                Toast.makeText(this, "Cancelled.", Toast.LENGTH_LONG).show();
-            } else {
-                Log.d("MainActivity", "Scanned");
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Try to scan bardcode",
-                        Toast.LENGTH_SHORT).show();
-                scanBarcode(view);
-            }
-        });
-
-
-        //testowanie api
-       // Product product = new Product(0, "Cola", "23123123");
-        TextView t = (TextView)findViewById(R.id.mainText);
-        ApiController api = new ApiController();
-        String listText = null;
-
-        ArrayList<ProductInStore> products = null;
-        try {
-            products = api.getConreteProductsByStoreID(1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String txt = null;
-        if(products!=null)
-        {
-            for(int i =0;i < products.size(); i++)
-            {
-                txt+=products.get(i).getName();
-            }
-        }
-
-        Product p = null;
-        try {
-            p = api.getProductByProducerCode("5900497310338");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        if(p!=null)t.setText(txt);
-        // testowanie api - koniec
-        //Toast.makeText(MainActivity.this, api.connect(),  Toast.LENGTH_LONG).show();
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void toScanner(View view){
+        Intent intencja = new Intent(this, Scanner.class);
+        startActivity(intencja);
+        //Toast.makeText(getApplicationContext(), "Kliknięto przycisk Login", Toast.LENGTH_SHORT).show();
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        switch(item.getItemId()){
-            case R.id.fab:
-                //Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //startActivityForResult(intentCamera, REQUEST_IMAGE_CAPTURE);
-                break;
-            case R.id.action_settings:Toast.makeText(getApplicationContext(),
-                    "Kliknięto przycisk Obiekt2",
-                    Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void toDatabase(View view){
+        Intent intencja = new Intent(this, Database.class);
+        startActivity(intencja);
+        //Toast.makeText(getApplicationContext(), "Kliknięto przycisk Login", Toast.LENGTH_SHORT).show();
     }
-
 
 }
